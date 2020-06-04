d2a8cd527d11ffbc748b15a5a014ffa9c5562891
Rimki <37412346+Rimki@users.noreply.github.com>
2020-06-04 17:18:08 +0900

    day19

---
+++ src/java_20200604/EmpDto.java
@@ -0,0 +1,94 @@
+package java_20200604;
+
+public class EmpDto {
+	private int no;
+	private String name;
+	private String job;
+	private int mgr;
+	private String hiredate;
+	private int sal;
+	private int comm;
+	private int deptNo;
+
+	public EmpDto(int no, String name, String job, int mgr, String hiredate, int sal, int comm, int deptNo) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.job = job;
+		this.mgr = mgr;
+		this.hiredate = hiredate;
+		this.sal = sal;
+		this.comm = comm;
+		this.deptNo = deptNo;
+	}
+
+	public EmpDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public int getMgr() {
+		return mgr;
+	}
+
+	public void setMgr(int mgr) {
+		this.mgr = mgr;
+	}
+
+	public String getHiredate() {
+		return hiredate;
+	}
+
+	public void setHiredate(String hiredate) {
+		this.hiredate = hiredate;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public int getComm() {
+		return comm;
+	}
+
+	public void setComm(int comm) {
+		this.comm = comm;
+	}
+
+	public int getDeptNo() {
+		return deptNo;
+	}
+
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+
+}
